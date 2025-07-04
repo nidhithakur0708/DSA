e2d7c950b5fb1264d73584a3a085a04b76d335ea
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2025-07-04 10:35:57 +0530

    Update BasicsCpp.cpp

--- BasicsCpp.cpp
+++ BasicsCpp.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    
+    //1.User input and output
+    
+    // cout<<"Hey Nidhi here"<<endl;
+    // int x,y;
+    // cin>>x;
+    // cout<<"Value of X is :"<<x<<" and y is:"<<y;
+    
+    //2.Data Types
+    /*
+    int var=34;
+    long-has a wider range
+    long long-more wider range
+    ->Remember the round of ranges to choose a certain data type.No need to remember the exact range!
+    
+    For decimals
+    float and double
+    
+    string and characters
+    ->getline(cin,string var) is used to take input for the strings with a space.
+    
+    char is used for a single character.
+    */
+    
+    // string str;
+    // char ch='a';
+    // getline(cin,str);
+    // cout<<str<<endl;
+    // cout<<ch;
+    
+    
+    return 0;
+    
+}
