ef8cfc80e20bad217f74a4869c14df7b711f5b61
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2025-07-07 14:58:18 +0530

    Update 5-Functions.cpp

--- 5-Functions.cpp
+++ 5-Functions.cpp
@@ -41,6 +41,14 @@ void doSome(string &s){
     cout<<"Inside the function "<<s<<endl;
 }
 
+//Arrays are always Pass by Reference Without &
+//Because the address is passed and not the value
+
+void arrayModify(int arr[],int n){
+    arr[0]+=100;
+    cout<<"The value inside function is:"<<arr[0]<<endl;
+}
+
 int main() {
     // printName("Shivam");
     // int num1,num2;
@@ -55,9 +63,17 @@ int main() {
 
     
     //Pass by Refernce ,&in the argument send the address for the variable and not the copy ,hence the function affects the original value itself
-    string s="nidhi";
-    doSome(s);
-    cout<<"In the main the function: "<<s<<endl;
+    // string s="nidhi";
+    // doSome(s);
+    // cout<<"In the main the function: "<<s<<endl;
+    
+    int n=5;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    arrayModify(arr,n);
+    cout<<"The value inside main:"<<arr[0]<<endl;
     
     return 0;
 }
