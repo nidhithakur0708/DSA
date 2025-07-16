9b9f9b022b2da33695f58828339c67d82502f883
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2025-07-16 12:35:11 +0530

    Update 7-Patterns.cpp

--- 7-Patterns.cpp
+++ 7-Patterns.cpp
@@ -50,9 +50,86 @@ void print6(int n){
         cout<<endl;
     }
 }
+void print7(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
+void print8(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
+void print10(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void print11(int n){
+    int start=1;
+    for(int i=0;i<n;i++){
+        if(i%2==0) start=1;
+        else start=0;
+        for(int j=0;j<=i;j++){
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+void print12(int n){
+    int space=2*n-2;
+    for(int i=1;i<=n;i++){
+        //number
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        //space
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        //number
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+        space-=2;
+    }
+    
+}
+
 int main(){
     int n;
     cin>>n;
-    print6(n);
-    
+    // print7(n);
+    // print8(n);
+    print12(n);
 }
+
