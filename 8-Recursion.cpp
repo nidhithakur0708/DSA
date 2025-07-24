cc526795fd59178338732338a43d2f7663f6bf31
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2025-07-24 12:56:14 +0530

    Update 8-Recursion.cpp

--- 8-Recursion.cpp
+++ 8-Recursion.cpp
@@ -119,6 +119,18 @@ int main(){
     string s="madam";
     cout<<check_palindrome(s,0);
 }
+//9.Fibnocaii number
+int fibo(int n){
+    if(n<=1) return n;
+    int last=fibo(n-1);
+    int slast=fibo(n-2);
+    return last+slast;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"The fibo at n is:"<<fibo(n);
+}
 
 
 
