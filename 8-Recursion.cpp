22718c9a1cc313c5abc35dae34e9580a795ddee2
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2025-07-24 12:11:35 +0530

    Update 8-Recursion.cpp

--- 8-Recursion.cpp
+++ 8-Recursion.cpp
@@ -109,6 +109,16 @@ int main(){
         cout<<arr[i]<<" ";
     }
 }
+//8.Check if string is a palindrome
+bool check_palindrome(string &s,int i){
+    if(i>=s.size()/2) return true;
+    if(s[i]!=s[s.size()-i-1]) return false;
+    return check_palindrome(s,i+1);
+}
+int main(){
+    string s="madam";
+    cout<<check_palindrome(s,0);
+}
 
 
 
